a6690527d8e2c4b93c470880f546a589d52e0453
Krittiya Lookyee Onbun-ua <lookyee.krittiya@bitkub.com>
2024-01-22 13:09:05 +0700

    feat: test
    test test test

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken: unknown;
+  }
+}
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
